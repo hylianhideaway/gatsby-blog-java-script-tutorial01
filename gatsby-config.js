7e1b89c25cf65a06c7512f12ee7c6167cf4654e2
Austin Steltz <austinsteltz@gmail.com>
2023-01-21 18:40:48 -0600

    Mia...

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,8 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
+
 
 
 // Step 2: Define your component
@@ -8,6 +10,11 @@ const IndexPage = () => {
   return (  
     <Layout pageTitle = "Welcome to my corner!">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Mia, a black and white lab mix, curled up on a couch cushion."
+        src="../images/MiaCurled.jpg"
+        layout="constrained"
+      />
     </Layout>
   )
 }
